24895469b7b2492d75765d139aee367a0194d07d
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-09 23:58:29 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
-	if (message.author.id === ayarlar.sahip) {
+	if (message.author.id === "355742603691687937") {
 		try {
 		  var code = args.join(" ");
 		  var evaled = eval(code);
