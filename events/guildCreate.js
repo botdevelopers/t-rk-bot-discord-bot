5d67072e0040aa32d23c5a1d1b04b993e51218ff
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-09 18:36:00 +0300

    Add files via upload

---
+++ events/guildCreate.js
@@ -0,0 +1,20 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+const girismesaj = [
+  '**Türk Bot Sunucunuza Eklendi!**',
+  '**Türk Bot** sunucunuzdaki insanlara kolaylıklar sağlar.',
+  'Bot Osman Tunahan Arıkan tarafından geliştirilmektedir!',
+  'Botumuzun özelliklerini öğrenmek için tr!yardım komutunu kullanabilirsin.',
+  '',
+  `**Türk Bot Discord Sunucusu** https://discord.gg/tzyauT5`
+]
+
+client.on('guildCreate', guild => {
+    const generalChannel = guild.defaultChannel
+    generalChannel.sendMessage(girismesaj)
+	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
+})
