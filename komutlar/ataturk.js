1dd4e7a982f29daf1f2132af0bf149f633e4dbd9
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-12 15:43:34 +0300

    Create ataturk.js

---
+++ komutlar/ataturk.js
@@ -0,0 +1,32 @@
+var request = require('request');
+
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+  
+request('https://api.eggsybot.xyz/ataturk', function (error, response, body) {
+    if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
+    else if (!error) { // Eğer hata yoksa;
+        var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
+        console.log(info.link); // ve konsola çıktıyı versin.
+    }
+});
+};
+
+exports.conf = {
+  enabled: true, //komutut açtık
+  guildOnly: false, //sadece servere özel yapmadık
+  aliases: ['ping','p'], //farklı çağrılar ekledik
+  permLevel: 0 //kimlerin kullanabileceğini yazdık (bot.js dosyasında perm leveller yazıyor)
+};
+
+exports.help = {
+  name: 'ping', //adını belirledik (kullanmak için gereken komut)
+  description: 'Botun pingini gösterir', //açıklaması
+  usage: 'ping' //komutun kullanım şekli (mesela hava <bölge>)
+};
+
+//komut taslağımız basit anlatımını zaten dosyalarda bulursunuz
+//bu ise bizim komut taslağımız
+//bunun üzerinden gideceğiz
