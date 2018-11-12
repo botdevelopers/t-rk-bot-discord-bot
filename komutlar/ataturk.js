2c88cd251ee48c0a040f988c16450ff62304657f
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-12 15:45:38 +0300

    Update ataturk.js

--- komutlar/ataturk.js
+++ komutlar/ataturk.js
@@ -17,14 +17,14 @@ request('https://api.eggsybot.xyz/ataturk', function (error, response, body) {
 exports.conf = {
   enabled: true, //komutut açtık
   guildOnly: false, //sadece servere özel yapmadık
-  aliases: ['ping','p'], //farklı çağrılar ekledik
+  aliases: ['ata','atatürk'], //farklı çağrılar ekledik
   permLevel: 0 //kimlerin kullanabileceğini yazdık (bot.js dosyasında perm leveller yazıyor)
 };
 
 exports.help = {
-  name: 'ping', //adını belirledik (kullanmak için gereken komut)
-  description: 'Botun pingini gösterir', //açıklaması
-  usage: 'ping' //komutun kullanım şekli (mesela hava <bölge>)
+  name: 'ataturk', //adını belirledik (kullanmak için gereken komut)
+  description: 'Rastgele Atatürk Resmi Gönderir.', //açıklaması
+  usage: 'ataturk' //komutun kullanım şekli (mesela hava <bölge>)
 };
 
 //komut taslağımız basit anlatımını zaten dosyalarda bulursunuz
