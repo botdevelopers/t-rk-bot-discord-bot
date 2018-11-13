0a0133f3442ca05e67d6413c67676fe41f8ab79c
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-13 18:27:29 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,4 @@
 module.exports = member => {
     let username = member.user.username;
     member.send('Hoş geldin **' + username + '**!');
-    member.member.guild.defaultChannel.send('Sunucumuza Hoşgeldin: ',username);
 };
