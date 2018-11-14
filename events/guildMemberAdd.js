425f97ca8d444976604700ccf9ea05bb1a52d896
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-14 14:18:37 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,7 @@
 module.exports = member => {
-    let username = member.user.username;
-    member.send('Sunucumuza Hoşgeldin **' + username + '**!');
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`:inbox_tray: Sunucuya Katıldı: **${member}** :inbox_tray:`);
 };
