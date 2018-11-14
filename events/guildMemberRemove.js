2636944f1d4003783c7a90445928e13622683aa5
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-14 14:19:20 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,7 @@
 module.exports = member => {
-  let guild = member.guild;
-  member.send('niye gittin?');
-  guild.defaultChannel.send(`${member.user.username} gitti.`);
+  const channel = member.guild.channels.find('name', 'hoşgeldiniz');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`:outbox_tray: Sunucudan Çıktı: **${member}** :outbox_tray:`);
 };
