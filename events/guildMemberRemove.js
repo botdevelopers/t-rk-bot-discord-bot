33e713326648c4c9617d85128a9e3e05a1a0d9cc
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-17 11:50:24 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,17 @@
 module.exports = member => {
+  const Discord = require('discord.js');
   const channel = member.guild.channels.find('name', 'hoşgeldiniz');
+  let memberavatar = member.user.avatarURL;
+  if (!channel) return;
+const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(memberavatar)
+  .setDescription(':outbox_tray: ' + member + ' | Sunucudan Ayrıldı! | Görüşürüz Bro!')
+  channel.sendEmbed(embed);
+  
+  /**const channel = member.guild.channels.find('name', 'hoşgeldiniz');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`:outbox_tray: Sunucudan Çıktı: **${member}** :outbox_tray:`);
+  channel.send(`:outbox_tray: Sunucudan Çıktı: **${member}** :outbox_tray:`); **/
 };
