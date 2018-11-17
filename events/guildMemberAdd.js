dbf21ee77ba3f599934f27e37ff7d755e4b93bdf
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-11-17 11:49:22 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,17 @@
 module.exports = member => {
+  const Discord = require('discord.js');
   const channel = member.guild.channels.find('name', 'hoşgeldiniz');
-  // Do nothing if the channel wasn't found on this server
+  let memberavatar = member.user.avatarURL;
+  if (!channel) return;
+const embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(memberavatar)
+  .setDescription(':inbox_tray: ' + member + ' | Sunucuya Katıldı! | Hoşgeldin Bro!')
+  channel.sendEmbed(embed);
+  
+  
+  /** Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`:inbox_tray: Sunucuya Katıldı: **${member}** :inbox_tray:`);
+  channel.send(`:inbox_tray: Sunucuya Katıldı: **${member}** :inbox_tray:`); **/
 };
